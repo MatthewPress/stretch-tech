24c30e6ea3caff5a487ea6d1e2ae1d7293400134
MatthewPress <press.matt14@gmail.com>
2022-10-29 14:26:20 -0600

    Build: create test data needed for landing page

---
+++ src/testData/emotionsData.js
@@ -0,0 +1,26 @@
+const emotionsData = {
+  "emotions": [
+    {
+      "id": 1,
+      "type": "anger"
+    },
+    {
+      "id": 2,
+      "type": "stress"
+    },
+    {
+      "id": 3,
+      "type": "fear"
+    },
+    {
+      "id": 4,
+      "type": "worry"
+    },
+    {
+      "id": 5,
+      "type": "doubt"
+    }
+  ]
+};
+
+export default emotionsData;
