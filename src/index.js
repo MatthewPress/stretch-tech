7e9bcae74fa43686a3c34c3cfc42cdd70000310b
MatthewPress <press.matt14@gmail.com>
2022-10-28 19:22:46 -0600

    Feat: add react router to project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App/App';
 
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
 );
 
