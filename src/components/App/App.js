4cbb45536d46a5e2cc6f65b2ba315b07bc14f654
MatthewPress <press.matt14@gmail.com>
2022-10-29 14:33:33 -0600

    Feat: set test emotion data to app state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+import emotionsData from '../../testData/emotionsData';
 import Header from '../Header/Header';
 
 import './App.css';
 
 function App() {
+  const [emotions, setEmotions] = useState([]);
+
+  useEffect(() => {
+    setEmotions(emotionsData);
+  }, []);
+
   return (
     <Header />
   );
--- src/testData/emotionsData.js
+++ src/testData/emotionsData.js
@@ -1,26 +1,24 @@
-const emotionsData = {
-  "emotions": [
-    {
-      "id": 1,
-      "type": "anger"
-    },
-    {
-      "id": 2,
-      "type": "stress"
-    },
-    {
-      "id": 3,
-      "type": "fear"
-    },
-    {
-      "id": 4,
-      "type": "worry"
-    },
-    {
-      "id": 5,
-      "type": "doubt"
-    }
-  ]
-};
+const emotionsData = [
+  {
+    "id": 1,
+    "type": "anger"
+  },
+  {
+    "id": 2,
+    "type": "stress"
+  },
+  {
+    "id": 3,
+    "type": "fear"
+  },
+  {
+    "id": 4,
+    "type": "worry"
+  },
+  {
+    "id": 5,
+    "type": "doubt"
+  }
+];
 
 export default emotionsData;
