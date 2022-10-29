7ba1ffe72f825c0dc12777147d78b57b7dc23416
MatthewPress <press.matt14@gmail.com>
2022-10-29 12:26:02 -0600

    Feat: add styles and logo to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,11 @@
+import logo from '../../assets/1200px-ghost-logo.svg.png';
+
 import './Header.css';
 
 function Header() {
   return (
     <header>
+      <img src={logo} alt="Cheers For Fears Ghost Logo" />
       <h1>Cheers For Fears</h1>
     </header>
   )
