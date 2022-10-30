60b6245e0b4c829fb39e831f10412fc197958dfe
MatthewPress <press.matt14@gmail.com>
2022-10-30 02:15:28 -0600

    Fix: add another form to fix routing path links

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import emotionsData from '../../testData/emotionsData';
 
 import Header from '../Header/Header';
 import Form from '../Form/Form';
+import ResourceQueryForm from '../ResourceQueryForm/ResourceQueryForm';
 
 import './App.css';
 
@@ -38,10 +39,11 @@ function App() {
             <Route
               exact path={`/:emotionType`}
               render={() =>
-                <Form 
+                <ResourceQueryForm 
                   message="Would you like some words of encouragement or coping strategies?" 
                   formFields={[{ id: 1, type: "words"}, { id: 2, type: "strategies"}]} 
                   handleSubmit={setRequestedResource}
+                  userEmotion={userEmotion}
                 />
               } 
             />
