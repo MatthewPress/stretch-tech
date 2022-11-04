1e5350968c3eba1e6a4db376033a3510db68be15
Anthony Shellman <atshellman@gmail.com>
2022-11-03 21:01:07 -0600

    Create function for posting

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,18 +1,11 @@
-// remove returnData when backend is available
-// const returnData = (path) => {
-//   return {ok: true, data: path};
-// }
 
 const getData = async (path) => {
   try {
-    // const response = await fetch(path);
     const response = await fetch(`http://localhost:3003/api/v1${path}`);
     if(!response.ok) {
       throw new Error();
     } else {
       const data = await response.json();
-      // const data = await response.data;
-      console.log("Api calls:", data)
       return data;
     }
   } catch (error) {
@@ -20,6 +13,27 @@ const getData = async (path) => {
   }
 }
 
+const postData = async (path, idNum, userString) => {
+  try {
+    const response = await fetch(`http://localhost:3003/api/v1${path}`, {
+      method: 'POST',
+      body: JSON.stringify(userString),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    if(!response.ok) {
+      throw new Error();
+    } else {
+      const data = await response.json();
+      return data;
+    }
+  } catch(error) {
+    return error;
+  }
+}
+
 export {
-  getData
+  getData,
+  postData
 };
