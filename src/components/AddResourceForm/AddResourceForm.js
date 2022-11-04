9a6a70c793dc82a91747c42ad81b702ed81360df
Anthony Shellman <atshellman@gmail.com>
2022-11-03 20:35:27 -0600

    Add pointer for buttons

--- src/components/AddResourceForm/AddResourceForm.js
+++ src/components/AddResourceForm/AddResourceForm.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
 import "./AddResourceForm.css"
 
 const AddResourceForm = () => {
@@ -9,18 +10,23 @@ const AddResourceForm = () => {
  
 
   return (
-    <form className='resource-form'>
-      <input 
-      type='text'
-      placeholder='Add resource'
-      name='resource'
-      value={useinput}
-      onChange={event => setUserInput(event.target.value)}
-      />
-      <button className='submit-button'>SUBMIT</button>
-    </form>
+    <section>
+      <h2 className='call-to-action'>Please enter new (blah)</h2>
+      <form className='resource-form'>
+        <input
+        type='text'
+        placeholder='Add resource'
+        name='resource'
+        value={useinput}
+        onChange={event => setUserInput(event.target.value)}
+        />
+        <button className='submit-button'>Submit</button>
+        <Link to='/'>
+          <button className='home-button'>Start Again</button>
+        </Link>
+      </form>
+    </section>
   )
-
 }
 
 export default AddResourceForm
