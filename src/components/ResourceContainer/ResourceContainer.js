20f3d153af69047331a7a5b3c646e7c41307e5d5
Anthony Shellman <atshellman@gmail.com>
2022-11-03 20:17:07 -0600

    Remove comment, minor refactor

--- src/components/ResourceContainer/ResourceContainer.js
+++ src/components/ResourceContainer/ResourceContainer.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import './ResourceContainer.css';
 
 function ResourceContainer({ resources, handleStartAgain, userEmotion, selectedResource }) {
-  console.log({resources})
   return (
     <article className="resource--container">
       <div className="resource-display--container">
@@ -15,7 +14,7 @@ function ResourceContainer({ resources, handleStartAgain, userEmotion, selectedR
             START AGAIN
           </button>
         </Link>
-        <Link to={`/${userEmotion.type}/${selectedResource.type}/addition`}>
+        <Link to={`/${userEmotion.name}/${selectedResource}/addition`}>
           <button className="resource--button" name="addMessage">
             ADD A MESSAGE
           </button>
